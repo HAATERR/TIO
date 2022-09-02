3c9ef2ff27ed17abf76bc770097849efa6d364b8
Enzo Reboredo <enzooreboredo@gmail.com>
2022-09-02 10:30:10 -0300

    tercer commit

---
+++ diariodigital.ej/index.php
@@ -0,0 +1,27 @@
+
+<?php require 'header.html';?>
+   
+<main class="container mt-5">
+
+      <?php
+      // incluyo el archivo que tiene el arreglo de noticias
+      require_once 'db_noticias.php'; 
+      ?>
+
+      <section class="noticias">
+        <?php foreach ($noticias as $key => $noticia) { ?>
+          <div class="card">
+              <img src="<?php echo $noticia->img ?>" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title"><?php echo $noticia->title?></h5>
+                <p class="card-text"><?php echo $noticia->text ?></p>
+                <a href="noticia.php?id=<?php echo $key ?>" class="btn btn-outline-primary">Leer más</a>
+              </div>
+            </div>
+            
+          <?php }?>
+      </section>
+        
+    </main>
+
+    <?php require 'footer.html'; ?>
